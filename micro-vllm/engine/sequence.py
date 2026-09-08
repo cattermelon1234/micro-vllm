97aa43cfa4d773cdb416f476630272606c209963
Brian Ling <brianling16@gmail.com>
2026-09-08 18:28:54 -0400

    finish seq

--- micro-vllm/engine/sequence.py
+++ micro-vllm/engine/sequence.py
@@ -8,24 +8,37 @@ class Status(Enum):
     FINISHED = auto()   # finished sequence (eos or max_tokens reached)
 
 class Sequence:
-    next_id = count();
+    block_size = 256
+    next_id = count()
+
     def __init__(self, token_ids: list[int]):
         self.seq_id = next(Sequence.next_id)  
         self.status = Status.WAITING
         self.token_ids = copy(token_ids)
-        self.num_tokens = len(token_ids)
-        self.num_prompt_tokens = len(token_ids)
-        self.num_scheduled_tokens = 0
-        self.num_cached_tokens =  0
+        self.num_tokens = len(token_ids)         # num tokens in seq so far
+        self.num_prompt_tokens = len(token_ids)  # num tokens in prompt
+        self.num_scheduled_tokens = 0            # num_tokens - num_cached_tokens
+        self.num_cached_tokens =  0              # num tokens we have in kv cache
         self.block_table = []
 
         self.is_prefill = True
         self.last_token = token_ids[-1]
         
-        self.block_size = sampling_params.block_size
         self.temperature = sampling_params.temperature
         self.max_tokens = sampling_params.max_tokens
         self.ignore_eos = sampling_params.ignore_eos
+
+    def __len__(self):
+        return len(self.token_ids)
+
+    def __getitem__(self, key):
+        return self.token_ids[key]
+
+    def is_finished(self):
+        return self.status == Status.FINISHED
+
+    def num_completion_tokens(self):
+        return self.num_tokens - self.num_prompt_tokens
 
     def num_blocks(self):
         return (self.num_tokens + self.block_size - 1) // self.block_size
@@ -33,4 +46,15 @@ class Sequence:
     def block(self, i):
         start = i * self.block_size 
         return self.token_ids[start : (start + self.block_size) % self.max_tokens]
+
+    def last_block_num_tokens(self):
+        return self.num_tokens - (self.num_blocks() - 1) * self.block_size
+
+    def append_token(self, token_id):
+        self.token_ids.append(token_id)
+        self.num_tokens += 1
+        self.last_token = token_id
+
+
+
 
